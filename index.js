a541c8eb9d0b6489e4430e2a6b9472c037298493
938938 <938kjh@gmail.com>
2022-02-08 23:41:59 +0900

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var text = document.getElementsByClassName("text");
+
+function btn1(){
+  text[0].innerHTML="모각코의 과제를"
+}
+function btn2(){
+  text[1].innerHTML="각잡고 수행합니다"
+}
+function btn3(){
+  text[2].innerHTML="코딩연습중!"
+}
