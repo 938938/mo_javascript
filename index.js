d810dfc24cbca279f0058e47ad66895c854e59cc
938938 <938kjh@gmail.com>
2022-02-14 23:37:04 +0900

    commit

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
-// var text = document.getElementsByClassName("text");
+// const x = 5;
+// const y = 3;
+// console.log("x =",x);
+// console.log("y = ",y);
+// console.log("x+y = ", x+y);
+// console.log("x-y = ", x-y);
+// console.log("x*y = ", x*y);
+// console.log("x/y = ", x/y);
 
-// function btn1(){
-//   text[0].innerHTML="모각코의 과제를"
-// }
-// function btn2(){
-//   text[1].innerHTML="각잡고 수행합니다"
-// }
-// function btn3(){
-//   text[2].innerHTML="코딩연습중!"
-// }
-
-// let n = 0;
-// function plus(){
-//   console.log(n++);
-// }
-
-// var num = 5;
-// var str = "얍";
-// var boo = true;
-// var n = null;
-// var un = undefined;
-// var ob = {first:"Hello", second:10};
-
-// console.log(num+" = "+typeof num);
-// console.log(str+" = "+typeof str);
-// console.log(boo+" = "+typeof boo);
-// console.log(n+" = "+typeof n);
-// console.log(un+" = "+typeof un);
-// console.log(ob+" = "+typeof ob);
-
-const x = 5;
-const y = 3;
-console.log("x =",x);
-console.log("y = ",y);
-console.log("x+y = ", x+y);
-console.log("x-y = ", x-y);
-console.log("x*y = ", x*y);
-console.log("x/y = ", x/y);
+function first(){
+  let x = document.getElementById("first").value;
+  console.log(x);
+}
+function second(){
+  let y = document.getElementById("second").value;
+  console.log(y);
+}
