9fe26dc2d2c738024e0acb0005a5606eb49e61e8
938938 <938kjh@gmail.com>
2022-02-18 22:20:40 +0900

    commit

--- index.js
+++ index.js
@@ -20,36 +20,47 @@
 //     span[0].style.color = colorCode;
 // }
 
-let title = document.getElementsByClassName("title");
-// let img = document.getElementsByClassName("img");
-let date = document.getElementsByClassName("date");
-let genre = document.getElementsByClassName("genre");
+// let title = document.getElementsByClassName("title");
+// // let img = document.getElementsByClassName("img");
+// let date = document.getElementsByClassName("date");
+// let genre = document.getElementsByClassName("genre");
 
 
-function movies(title, date, genre){
-    this.title = title,
-    // this.image = image,
-    this.date = date,
-    this.genre = genre
-}
+// function movies(title, date, genre){
+//     this.title = title,
+//     // this.image = image,
+//     this.date = date,
+//     this.genre = genre
+// }
 
-let movieOne = new movies("Sister Act" , "1993", "코미디/드라마");
-let movieTwo = new movies("Mamma Mia!", "2008", "코미디/뮤지컬");
-let movieThree = new movies("Aladdin", "2019", "판타지/뮤지컬");
+// let movieOne = new movies("Sister Act" , "1993", "코미디/드라마");
+// let movieTwo = new movies("Mamma Mia!", "2008", "코미디/뮤지컬");
+// let movieThree = new movies("Aladdin", "2019", "판타지/뮤지컬");
 
-function one(){
-    title[0].innerHTML = movieOne.title;
-    // img.src = movieOne.image;
-    date[0].innerHTML = movieOne.date;
-    genre[0].innerHTML = movieOne.genre;
-}
-function two(){
-    title[0].innerHTML = movieTwo.title;
-    date[0].innerHTML = movieTwo.date;
-    genre[0].innerHTML = movieTwo.genre;
-}
-function three(){
-    title[0].innerHTML = movieThree.title;
-    date[0].innerHTML = movieThree.date;
-    genre[0].innerHTML = movieThree.genre;
-}
+// function one(){
+//     title[0].innerHTML = movieOne.title;
+//     // img.src = movieOne.image;
+//     date[0].innerHTML = movieOne.date;
+//     genre[0].innerHTML = movieOne.genre;
+// }
+// function two(){
+//     title[0].innerHTML = movieTwo.title;
+//     date[0].innerHTML = movieTwo.date;
+//     genre[0].innerHTML = movieTwo.genre;
+// }
+// function three(){
+//     title[0].innerHTML = movieThree.title;
+//     date[0].innerHTML = movieThree.date;
+//     genre[0].innerHTML = movieThree.genre;
+// }
+
+setTimeout(function onClick(){
+    const name = document.getElementsByTagName("h2");
+    name[0].innerText = "김OO";
+    const p = document.getElementsByTagName("p");
+    p[0].innerText = "010-XXXX-XXXX";
+    const btn = document.getElementById("btn");
+    btn.innerText = "연결중…";
+},2000);
+
+document.getElementById("btn").addEventListener("click",onClick);
