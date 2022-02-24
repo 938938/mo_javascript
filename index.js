cec4e6c4280d0fb5c2c0590345f013f9b18e097a
938938 <938kjh@gmail.com>
2022-02-25 03:01:12 +0900

    commit

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let n = 0;
-
 let json = [
     {
         "question":"안녕?",
@@ -33,6 +31,7 @@ typewriter
 let question =""; //질문 임시 저장 함수
 let answer=""; // 대답 임시 저장 함수
 
+let n = 0; // 따라하기 판단
 let b = 0; // 다크모드 판단
 let key=0; // 말가르쳐주기 판단
 
@@ -44,17 +43,7 @@ function text_check(){
     const bod = document.getElementById("body");
     const cat = document.getElementById("cat");
     
-
-    //length = 배열 길이 확인 함수, json 데이터를 모두 확인.
-    for(let i = 0; i<json.length; i++){
-        if(value==json[i].question){
-            reply.innerText=json[i].answer;
-            input.value=null;
-            return;
-        }
-    }
-
-    if(n==0&&value=="따라하기"){
+    if(value=="따라하기"&&n==0){
         typewriter
         .deleteAll()
         .typeString("응! 이제 따라할게!")
@@ -86,11 +75,21 @@ function text_check(){
         return;
     };
 
+    //length = 배열 길이 확인 함수, json 데이터를 모두 확인.
+    for(let i = 0; i<json.length; i++){
+        if(value==json[i].question){
+            reply.innerText=json[i].answer;
+            input.value=null;
+            return;
+        }
+    }
+
     //말 배우기 함수.
     function learning(){
         reply.innerText="나한테 가르쳐줄래?(응 / 싫어)";
         question=value; //사용자 질문 미리 저장
         key=1; //조건문으로 진입하는 키 값 변경
+        return;
     }
 
     if(key==1){
@@ -98,25 +97,26 @@ function text_check(){
             reply.innerText="대답을 알려줘!";
             input.value=null;
             key=2;
+            return;
         } else {
             reply.innerText="야옹?";
             input.value=null;
             key=0;
+            return;
         }
-        return;
     }
 
     function push_json(){
         json.push({question: `${question}`, answer: `${answer}`});
         //json이라는 데이터에 값을 추가하는 함수
         reply.innerText="기억했어!";
-        key = 0;
     }
 
     //answer값에 사용자 입력 저장
     if(key==2){
         answer=value;
         push_json();
+        key = 0;
         input.value=null;
         return;
     }
@@ -128,10 +128,10 @@ function text_check(){
             cat.style.filter="grayscale(100%)";
             b++;
             input.value=null;
-            return;
         } else {
             reply.innerText="이미 어두운걸?";
             input.value=null;
+            return;
         };
     } else if (value=="밝게"){
         if(b==1){
@@ -140,16 +140,17 @@ function text_check(){
             cat.style.filter="";
             b=0;
             input.value=null;
-            return;
         } else {
             reply.innerText="지금도 밝아!";
             input.value=null;
+            return;
         };
     } else {
         if(key==0){
             reply.innerText="모르는 말이야!";
             setTimeout(learning,2000);
             input.value=null;
+            return;
         }
     };
 }
