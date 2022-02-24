d32bf3c843a5d3f51afdc8d6a8558e6b56005041
938938 <938kjh@gmail.com>
2022-02-25 01:25:04 +0900

    commit

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 let n = 0;
 
+let json = [
+    {
+        "question":"안녕?",
+        "answer":"안녕!"
+    },
+    {
+        "question":"밥",
+        "answer":"좋아!"
+    },
+    {
+        "question":"뭐해?",
+        "answer":"놀고있어!"
+    }
+]
+
 const app = document.getElementById("type");
 const typewriter = new Typewriter(app, {
     loop: false,
@@ -10,69 +25,51 @@ typewriter
     .typeString("야옹")
     .pauseFor(300)
     .deleteAll()
-    .typeString("이제부터 대답할거에냥")
+    .typeString("이야기를 하자!")
     .pauseFor(300)
     .deleteAll()
     .start();
 
-function output(){
+let question ="";
+let answer="";
+
+let key=0;
+
+function text_check(){
     event.preventDefault();
-    let value = document.getElementById("input").value;
-    const input = document.getElementById("input");
-    const btn = document.getElementById("btn");
-    const say = document.getElementById("type");
+    const value=document.getElementById("input").value;
+    const reply = document.getElementById("type");
+
     const bod = document.getElementById("body");
     const cat = document.getElementById("cat");
     const ti = document.getElementById("ti");
-    if(value=="안녕?"){
-        typewriter
-        .deleteAll()
-        .typeString("안녕!")
-        .pauseFor(500)
-        .deleteAll()
-        .start();
-        // say.innerText = "안녕!";
-    } else if(value=="배경색"){
-        bod.style.backgroundColor = "azure";
-    } else if(value=="원래대로"){
-        bod.style.backgroundColor = "white";
-    } else if (value=="크게"){
-        say.style.fontSize="20px";
-    } else if (value=="작게"){
-        say.style.fontSize="10px";
-    } else if (value=="기본"){
-        say.style.fontSize="15px";
-    } else if (value==""){
-        alert("메세지를 입력해주세요!");
-    } else if (value=="처음"){
-        // say.innerText ="야옹";
-        typewriter
-        .deleteAll()
-        .typeString("야옹")
-        .pauseFor(500)
-        .deleteAll()
-        .start();
-    } else if (value=="어둡게"){
-        bod.style.backgroundColor = "black";
-        say.style.color="white";
-        cat.style.filter="grayscale(100%)";
-        btn.style.backgroundColor="white";
-        ti.style.backgroundColor="white";
-    } else if (value=="밝게"){
-        bod.style.backgroundColor = "white";
-        say.style.color="black";
-        cat.style.filter="";
-    } else if (value=="밥"){
-        typewriter
-        .deleteAll()
-        .typeString("좋아!")
-        .pauseFor(500)
-        .deleteAll()
-        .start();
-        // say.innerText="좋아!";
-    } else {
-        console.log(value);
-    };
+    
+    for(let i = 0; i<json.length; i++){
+        if(value==json[i].question){
+            reply.innerText=json[i].answer;
+            input.value=null;
+            return;
+        }
+    }
+
+    if(key==1){
+        if(value=="네"){
+            reply.innerText="대답을 입력해주세요!";
+            key=2;
+        } else {
+            reply.innerText="야옹?";
+            key=0;
+        }
+        return;
+    }
+
+    if(key==2){
+        answer=value;
+        push_json();
+        reply.innerText="기억했어!";
+        return;
+    }
+
     if(n==0&&value=="따라하기"){
         typewriter
         .deleteAll()
@@ -80,8 +77,9 @@ function output(){
         .pauseFor(500)
         .deleteAll()
         .start();
-        // say.innerText="응! 이제 따라할게!"
         n=1;
+        input.value=null;
+        return;
     } else if (n==1&&value!=="그만하자") {
         typewriter
         .deleteAll()
@@ -89,7 +87,8 @@ function output(){
         .pauseFor(500)
         .deleteAll()
         .start();
-        // say.innerText=" “ "+value+" ! ” ";
+        input.value=null;
+        return;
     };
     if (n==1&&value=="그만하자"){
         typewriter
@@ -98,13 +97,46 @@ function output(){
         .pauseFor(500)
         .deleteAll()
         .start();
-        // say.innerText="알았어!";
+        input.value=null;
         n=0;
+        return;
     };
 
-    input.value=null;
+    if (value=="어둡게"){
+        reply.innerText="불을 껐어!"
+        bod.style.backgroundColor = "black";
+        cat.style.filter="grayscale(100%)";
+    } else if (value=="밝게"){
+        reply.innerText="불을 켰어!"
+        bod.style.backgroundColor = "white";
+        cat.style.filter="";
+    } else {
+        if(key==0){
+            reply.innerText="모르는 말이야!"
+            setTimeout(learning,2000);
+            question=value;
+            key=1;
+        }
+    }
+
 }
 
+function learning(){
+    reply.innerText="나한테 가르쳐줄래?(네 / 아니오)"
+}
 
-document.getElementById("btn").addEventListener("click",output);
-document.getElementById("form").addEventListener("submit",output);
+function push_json(){
+	json.push({question: `${question}`, answer: `${answer}`});
+	key = 0;
+}
+
+// function push_json(){
+//     json.push({question:`${question}`,answer:`${answer}`});
+//     document.getElementById("type").innerHTML = "기억했어!";
+//     key=0;
+// }
+
+
+
+document.getElementById("btn").addEventListener("click",text_check);
+document.getElementById("form").addEventListener("submit",text_check);
